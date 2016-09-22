6129aa6ebba459c3f12a8591cd5da6544cd30363
Daniel Mangowi <danielmangowi@gmail.com>
2016-09-23 01:01:25 +0200

    Signup page and serving to the user details to the database

--- routes.py
+++ routes.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
+##mysql://username:password@server/db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
+db.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
-
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -12,6 +16,22 @@ def index():
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+  form = SignupForm()
+
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      return 'Success!'
+
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
   app.run(debug=True)
