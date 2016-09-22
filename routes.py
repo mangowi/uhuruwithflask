df30f4f4cff415ea806cf77a751f830eebb6204e
Daniel Mangowi <danielmangowi@gmail.com>
2016-09-22 21:38:55 +0200

    Changes on the route.py to map about.html

--- routes.py
+++ routes.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/about")
 def about():
-  return render_template("index.html")
+  return render_template("about.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
