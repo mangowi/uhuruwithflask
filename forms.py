d49cb7f519730d76f7e7ab79ae3d8b746cb05064
Daniel Mangowi <danielmangowi@gmail.com>
2016-09-23 01:36:49 +0200

    Creating user session, logining, logingout

--- forms.py
+++ forms.py
@@ -8,3 +8,9 @@ class SignupForm(Form):
   email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
   password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
   submit = SubmitField('Sign up')
+
+# For login
+class LoginForm(Form):
+  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+  password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
+  submit = SubmitField("Sign in")
