d7c5430ce099b695ee859238df68e00bce1b70f4
Daniel Mangowi <danielmangowi@gmail.com>
2016-09-22 22:17:56 +0200

    Adding models py and connectiong to datbase using SQLALCHEMY database

--- routes.py
+++ routes.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+
+
 @app.route("/")
 def index():
   return render_template("index.html")
